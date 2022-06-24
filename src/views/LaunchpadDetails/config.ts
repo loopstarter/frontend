7addb8244b3323715e1c6c576e296852b865aecb
Nguyễn Văn Dũng <hos6@altisss.vn>
2022-06-24 23:16:00 +0700

    Update IDO

--- src/views/LaunchpadDetails/config.ts
+++ src/views/LaunchpadDetails/config.ts
@@ -264,7 +264,7 @@ export const configIDO: IConfigIDO[] = [
     }
   },
   {
-    pid: 5,
+    pid: 3,
     mainContractIDO: "0xcb987B25679c47Fc3BB6A713d5f9Fc3D1242469d",
     hidden: false,
     projectDescription: 'WalkN exists with only one mission - To bring people, fitness and crypto together by building a solid platform that rewards people to take care of their fitness. WalkN provides a platform for improving one\'s fitness while earning rewards in cryptocurrency.It leverages the newly born trend, Move2Earn.The app users purchase an NFT of their choice, start the app, and earn money with every step they take.',
@@ -293,7 +293,7 @@ export const configIDO: IConfigIDO[] = [
       timeDistribute: '25/06/2022',
       timeDistributeEnd: '26/06/2022',
       timeDistributeListing: '27/06/2022',
-      allocation: '100 BUSD',
+      allocation: '500 BUSD',
       allocationSize: ' - ',
       listingPrice: '$0.0049',
       projectName: 'WalkN App I Walk, Jog, or Run & Earn Rewards!',
@@ -301,11 +301,11 @@ export const configIDO: IConfigIDO[] = [
       symbol: 'WALKN',
       totalSupply: '3,000,000,000',
       access: 'Public',
-      roundType: 'FCFS round',
+      roundType: 'Guaranteed round',
       linkWhiteList: 'https://sweepwidget.com/view/59279-8ltinjxv',
       logoWidth: 120,
       startTime: 1656162000,
-      totalSales: 29000
+      totalSales: 35000
     }
   },
   {
@@ -354,7 +354,7 @@ export const configIDO: IConfigIDO[] = [
     }
   },
   {
-    pid: 3,
+    pid: 5,
     mainContractIDO: "0xcb987B25679c47Fc3BB6A713d5f9Fc3D1242469d",
     hidden: false,
     projectDescription: 'WalkN exists with only one mission - To bring people, fitness and crypto together by building a solid platform that rewards people to take care of their fitness. WalkN provides a platform for improving one\'s fitness while earning rewards in cryptocurrency.It leverages the newly born trend, Move2Earn.The app users purchase an NFT of their choice, start the app, and earn money with every step they take.',
@@ -383,7 +383,7 @@ export const configIDO: IConfigIDO[] = [
       timeDistribute: '25/06/2022',
       timeDistributeEnd: '26/06/2022',
       timeDistributeListing: '27/06/2022',
-      allocation: '500 BUSD',
+      allocation: '100 BUSD',
       allocationSize: ' - ',
       listingPrice: '$0.0049',
       projectName: 'WalkN App I Walk, Jog, or Run & Earn Rewards!',
@@ -391,11 +391,11 @@ export const configIDO: IConfigIDO[] = [
       symbol: 'WALKN',
       totalSupply: '3,000,000,000',
       access: 'Public',
-      roundType: 'Guaranteed round',
+      roundType: 'FCFS round',
       linkWhiteList: 'https://sweepwidget.com/view/59279-8ltinjxv',
       logoWidth: 120,
       startTime: 1656162000,
-      totalSales: 35000
+      totalSales: 29000
     }
-  }
+  },
 ]
