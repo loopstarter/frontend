b1dcabd1e3b9f300742d081d36d67c04172ffeba
Nguyễn Văn Dũng <hos6@altisss.vn>
2022-06-24 22:13:07 +0700

    Update IDO

--- src/views/LaunchpadDetails/config.ts
+++ src/views/LaunchpadDetails/config.ts
@@ -84,52 +84,52 @@ export const configIDO: IConfigIDO[] = [
   //     startTime: 1654707600
   //   },
   // },
-  // {
-  //   pid: 0,
-  //   mainContractIDO: "0xc972bC7A8Afc4Da883AEE52429f569f5002E71dA",
-  //   hidden: true,
-  //   projectDescription: 'Acadex Network is the World’s Pioneering Decentralized Education Network, introducing blockchain’s first Proof of Knowledge (PoK) consensus.',
-  //   projectShortDescription:
-  //     'Acadex Network is the World’s Pioneering Decentralized Education Network, introducing blockchain’s first Proof of Knowledge (PoK) consensus.',
-  //   projectLogo: '/images/app-ido/acdx.png',
-  //   social: {
-  //     twitter: '',
-  //     website: 'https://acadex.network/',
-  //   },
-  //   viewTokenLink: 'https://acadex.network/',
-  //   tokenInfo: {
-  //     sell: {
-  //       symbol: tokens.acdx.symbol,
-  //       address: tokens.acdx.address,
-  //       decimals: tokens.acdx.decimals,
-  //     },
-  //     useForBuy: {
-  //       symbol: 'BUSD',
-  //       address: tokens.busd.address,
-  //       decimals: tokens.busd.decimals,
-
-  //     },
-  //   },
-  //   projectInfo: {
-  //     timeDistribute: '08/06/2022',
-  //     timeDistributeEnd: '--/--/2022',
-  //     timeDistributeListing: '--/--/2022',
-  //     allocation: '100 BUSD',
-  //     allocationSize: ' - ',
-  //     listingPrice: '$0.003',
-  //     projectName: 'PIONEERING DECENTRALIZED EDUCATION NETWORK',
-  //     projectNameShort: 'Acadex Network',
-  //     symbol: 'ACDX',
-  //     demicals: tokens.acdx.decimals,
-  //     totalSupply: '1,000,000,000',
-  //     addressToken: tokens.acdx.address,
-  //     access: 'Public',
-  //     logoWidth: 120,
-  //     startTime: 1654682400
-  //   }
-  // },
   {
-    pid: 3,
+    pid: 0,
+    mainContractIDO: "0xc972bC7A8Afc4Da883AEE52429f569f5002E71dA",
+    hidden: true,
+    projectDescription: 'Acadex Network is the World’s Pioneering Decentralized Education Network, introducing blockchain’s first Proof of Knowledge (PoK) consensus.',
+    projectShortDescription:
+      'Acadex Network is the World’s Pioneering Decentralized Education Network, introducing blockchain’s first Proof of Knowledge (PoK) consensus.',
+    projectLogo: '/images/app-ido/acdx.png',
+    social: {
+      twitter: '',
+      website: 'https://acadex.network/',
+    },
+    viewTokenLink: 'https://acadex.network/',
+    tokenInfo: {
+      sell: {
+        symbol: tokens.acdx.symbol,
+        address: tokens.acdx.address,
+        decimals: tokens.acdx.decimals,
+      },
+      useForBuy: {
+        symbol: 'BUSD',
+        address: tokens.busd.address,
+        decimals: tokens.busd.decimals,
+
+      },
+    },
+    projectInfo: {
+      timeDistribute: '08/06/2022',
+      timeDistributeEnd: '--/--/2022',
+      timeDistributeListing: '--/--/2022',
+      allocation: '100 BUSD',
+      allocationSize: ' - ',
+      listingPrice: '$0.003',
+      projectName: 'PIONEERING DECENTRALIZED EDUCATION NETWORK',
+      projectNameShort: 'Acadex Network',
+      symbol: 'ACDX',
+      demicals: tokens.acdx.decimals,
+      totalSupply: '1,000,000,000',
+      addressToken: tokens.acdx.address,
+      access: 'Public',
+      logoWidth: 120,
+      startTime: 1654682400
+    }
+  },
+  {
+    pid: 5,
     mainContractIDO: "0xcb987B25679c47Fc3BB6A713d5f9Fc3D1242469d",
     hidden: false,
     projectDescription: 'WalkN exists with only one mission - To bring people, fitness and crypto together by building a solid platform that rewards people to take care of their fitness. WalkN provides a platform for improving one\'s fitness while earning rewards in cryptocurrency.It leverages the newly born trend, Move2Earn.The app users purchase an NFT of their choice, start the app, and earn money with every step they take.',
@@ -219,7 +219,7 @@ export const configIDO: IConfigIDO[] = [
     }
   },
   {
-    pid: 5,
+    pid: 3,
     mainContractIDO: "0xcb987B25679c47Fc3BB6A713d5f9Fc3D1242469d",
     hidden: false,
     projectDescription: 'WalkN exists with only one mission - To bring people, fitness and crypto together by building a solid platform that rewards people to take care of their fitness. WalkN provides a platform for improving one\'s fitness while earning rewards in cryptocurrency.It leverages the newly born trend, Move2Earn.The app users purchase an NFT of their choice, start the app, and earn money with every step they take.',
